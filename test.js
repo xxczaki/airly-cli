ff8c76964a984f754662892ac44d52a94ab8be41
Antoni Kepinski <hello@akepinski.me>
2018-11-05 20:07:38 +0100

    wtf

--- test.js
+++ test.js
@@ -6,11 +6,6 @@ test('Help message', async t => {
 	t.regex(ret.stdout, /Usage/);
 });
 
-test('Version number', async t => {
-	const {stdout} = await execa.shell('node cli.js --version');
-	t.true(stdout.length < 6);
-});
-
 test('Reset API key', async t => {
 	const ret = await execa.shell('node cli.js --reset');
 	t.regex(ret.stdout, /API key deleted/);
